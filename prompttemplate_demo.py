b5b66e6ad7069cd4569d8dcac4e9084450cade95
bhsourav21 <bhsourav21@gmail.com>
2026-03-03 19:48:48 -0800

    added number_input for streamlit

--- prompttemplate_demo.py
+++ prompttemplate_demo.py
@@ -12,14 +12,16 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
 prompt = PromptTemplate(
-    input_variables=["country1", "country2"],
-    template="""What is the capital of {country1} and {country2}?"""
+    input_variables=["country1", "country2", "number_of_sentences"],
+    template="""What is the capital of {country1} and {country2}?
+    Write {number_of_sentences} sentences on both mentioning why they are famous."""
 )
 
 country1 = st.text_input("Enter the first country: ")
 country2 = st.text_input("Enter the second country: ")
+number_of_sentences = st.number_input("Enter the number of sentences: ", min_value=1, max_value=10, value=1)
 
 if country1 and country2:
-    prompt_value = prompt.format(country1=country1, country2=country2)
+    prompt_value = prompt.format(country1=country1, country2=country2, number_of_sentences=number_of_sentences)
     response = llm.invoke(prompt_value)
     st.write(response.content)
