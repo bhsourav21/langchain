169ee49dcbe615f991bdb20d25a87cab0035fc9f
bhsourav21 <bhsourav21@gmail.com>
2026-03-04 20:16:39 -0800

    regular sequential chaining demo

---
+++ sequential_chain_demo.py
@@ -0,0 +1,43 @@
+import os
+import json
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+prompt_title = PromptTemplate(
+    input_variables=["topic"],
+    template="""You are an experienced speech writer.
+    You need to craft an impactful title for a speech on the following topic: {topic}
+    Answer exactly with one title.
+    """
+)
+
+prompt_speech = PromptTemplate(
+    input_variables=["title", "emotion"],
+    template="""You need to write a powerful {emotion} speech of 350 words for the following title:
+    {title}
+    """
+)
+
+first_chain = prompt_title | llm | StrOutputParser() | (lambda title: (st.write(title), title)[1])
+second_chain = prompt_speech | llm
+final_chain = first_chain | (lambda title: {"title": title, "emotion": emotion}) | second_chain
+
+st.title("Speech Generator")
+topic = st.text_input("Enter the topic:")
+emotion = st.text_input("Enter the emotion:")
+
+if topic and emotion:
+    response = final_chain.invoke({
+        "topic": topic,
+        "emotion": emotion
+    })
+    
+    st.write(response.content)
