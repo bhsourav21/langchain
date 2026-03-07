e484637e984470bd3e958e806810d7365687f09d
bhsourav21 <bhsourav21@gmail.com>
2026-03-07 13:15:19 -0800

    embeddings

---
+++ embeddings/embeddings_demo.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm = OpenAIEmbeddings()
+
+text=input("Enter a text: ")
+response = llm.embed_query(text)
+print(response)
---
+++ embeddings/similarity_finder.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+import numpy as np
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm = OpenAIEmbeddings()
+
+text1 =input("Enter a text1: ")
+text2 =input("Enter a text2: ")
+response1 = llm.embed_query(text1)
+response2 = llm.embed_query(text2)
+similarity_score = np.dot(response1, response2)
+
+print(f"similarity_score (higher the number, its more similar):{similarity_score}")
